dd16a5e4dc40a8c28e12e97743a2bca2e31d64ae
Kevin Brock <ikatowi@gmail.com>
2020-02-19 17:48:52 -0500

    continuing to look for source of disconnect re: implementing customized plugin, momentarily turning to traits.erb

--- plugins/traits/plugin/templates/traits_template.rb
+++ plugins/traits/plugin/templates/traits_template.rb
@@ -9,6 +9,14 @@ module AresMUSH
         @traits = traits
         super File.dirname(__FILE__) + "/traits.erb"
       end
+      
+      def categories
+        Traits.relationships_by_category(category)
+      end
+      
+      def sorted_relationships(category)
+        category.sort_by { |name, desc| [ desc['order'] || 99, name ] }
+      end
     end
   end
 end
